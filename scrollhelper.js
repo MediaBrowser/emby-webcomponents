5ab75acaf703571a7f39768b21babfe4c747fbcf
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-29 00:30:28 -0500

    center on focus

---
+++ scrollhelper.js
@@ -0,0 +1,89 @@
+define(['focusManager'], function (focusManager) {
+
+    function getOffset(elem) {
+
+        var doc = document;
+        var box = { top: 0, left: 0 };
+
+        if (!doc) {
+            return box;
+        }
+
+        var docElem = doc.documentElement;
+
+        // Support: BlackBerry 5, iOS 3 (original iPhone)
+        // If we don't have gBCR, just use 0,0 rather than error
+        if (elem.getBoundingClientRect) {
+            box = elem.getBoundingClientRect();
+        }
+        var win = doc.defaultView;
+        return {
+            top: box.top + win.pageYOffset - docElem.clientTop,
+            left: box.left + win.pageXOffset - docElem.clientLeft
+        };
+    }
+
+    function getPosition(scrollContainer, item, horizontal) {
+        var slideeOffset = getOffset(scrollContainer);
+        var itemOffset = getOffset(item);
+
+        var offset = horizontal ? itemOffset.left - slideeOffset.left : itemOffset.top - slideeOffset.top;
+        var size = item[horizontal ? 'offsetWidth' : 'offsetHeight'];
+
+        if (horizontal) {
+            offset += scrollContainer.scrollLeft;
+        } else {
+            offset += scrollContainer.scrollTop;
+        }
+
+        var frameSize = horizontal ? scrollContainer.offsetWidth : scrollContainer.offsetHeight;
+
+        return {
+            start: offset,
+            center: (offset - (frameSize / 2) + (size / 2)),
+            end: offset - frameSize + size,
+            size: size
+        };
+    }
+
+    function centerOnFocus(e, scrollSlider, horizontal) {
+        var focused = focusManager.focusableParent(e.target);
+
+        if (focused) {
+            var pos = getPosition(scrollSlider, focused, horizontal);
+
+            if (horizontal) {
+                scrollSlider.scrollTo(pos.center, 0);
+            } else {
+                scrollSlider.scrollTo(0, pos.center);
+            }
+        }
+    }
+
+    function centerOnFocusHorizontal(e) {
+        centerOnFocus(e, this, true);
+    }
+    function centerOnFocusVertical(e) {
+        centerOnFocus(e, this, false);
+    }
+
+    return {
+        getPosition: getPosition,
+        centerFocus: {
+            on: function(element, horizontal) {
+                if (horizontal) {
+                    element.addEventListener('focus', centerOnFocusHorizontal, true);
+                } else {
+                    element.addEventListener('focus', centerOnFocusVertical, true);
+                }
+            },
+            off: function (element, horizontal) {
+                if (horizontal) {
+                    element.removeEventListener('focus', centerOnFocusHorizontal, true);
+                } else {
+                    element.removeEventListener('focus', centerOnFocusVertical, true);
+                }
+            }
+        }
+    };
+});
