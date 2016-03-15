8243c2df7fab1b917de6084922f718a77ca53daa
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-15 02:19:57 -0400

    update page event

--- viewmanager.js
+++ viewmanager.js
@@ -20,7 +20,7 @@ define(['viewcontainer', 'focusManager', 'queryString', 'connectionManager', 'ev
                 // Use controller method
                 var controller = new options.controllerFactory(newView, eventDetail.detail.params);
             } else if (dispatchPageEvents) {
-                dispatchViewEvent(newView, 'pageinit');
+                dispatchViewEvent(newView, 'viewinit');
             }
         }
 
