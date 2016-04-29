3f280e13a6e299d788a299b1eaeed9b0cdab5d85
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-04-28 22:47:53 -0400

    update sharingmenu.js

--- sharing/sharingmenu.js
+++ sharing/sharingmenu.js
@@ -16,9 +16,7 @@
         html += '<div class="ssk-group ssk-round ssk-lg">';
 
         // We can only do facebook if we can guarantee that the current page is available over the internet, since FB will try to probe it.
-        if (Dashboard.isConnectMode()) {
-            html += '<a href="" class="ssk ssk-facebook"></a>';
-        }
+        html += '<a href="" class="ssk ssk-facebook"></a>';
 
         html += '<a href="" class="ssk ssk-twitter"></a><a href="" class="ssk ssk-google-plus"></a><a href="" class="ssk ssk-pinterest"></a><a href="" class="ssk ssk-tumblr"></a></div>';
         html += '</div>';
