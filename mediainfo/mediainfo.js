85338f50ee3c3e7657cd35c2824aaa177ee891f6
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-12 00:53:30 -0400

    update media info

--- mediainfo/mediainfo.js
+++ mediainfo/mediainfo.js
@@ -158,17 +158,17 @@ define(['datetime', 'globalize', 'embyRouter', 'html!./../icons/mediainfo.html',
 
             if (item.IsLive) {
                 miscInfo.push({
-                    html: '<span class="liveTvProgram">' + globalize.translate('core#AttributeLive') + '</span>'
+                    html: '<span class="mediaInfoProgramAttribute mediaInfoItem">' + globalize.translate('core#AttributeLive') + '</span>'
                 });
             }
             else if (item.IsPremiere) {
                 miscInfo.push({
-                    html: '<span class="premiereTvProgram">' + globalize.translate('core#AttributePremiere') + '</span>'
+                    html: '<span class="mediaInfoProgramAttribute mediaInfoItem">' + globalize.translate('core#AttributePremiere') + '</span>'
                 });
             }
             else if (item.IsSeries && !item.IsRepeat) {
                 miscInfo.push({
-                    html: '<span class="newTvProgram">' + globalize.translate('core#AttributeNew') + '</span>'
+                    html: '<span class="mediaInfoProgramAttribute mediaInfoItem">' + globalize.translate('core#AttributeNew') + '</span>'
                 });
             }
 
