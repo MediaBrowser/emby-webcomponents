69a65809b308d45535a7dcf5eda9b58e3379c28c
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-01-03 03:31:19 -0500

    update duration method

--- playback/playbackmanager.js
+++ playback/playbackmanager.js
@@ -1212,7 +1212,19 @@ define(['events', 'datetime', 'appSettings', 'pluginManager', 'userSettings', 'g
                 return player.duration();
             }
 
-            return getPlayerData(player).streamInfo.mediaSource.RunTimeTicks || ((player.duration() || 0) * 10000);
+            var streamInfo = getPlayerData(player).streamInfo;
+
+            if (streamInfo && streamInfo.mediaSource && streamInfo.mediaSource.RunTimeTicks) {
+                return streamInfo.mediaSource.RunTimeTicks;
+            }
+
+            var playerDuration = player.duration();
+
+            if (playerDuration) {
+                playerDuration *= 10000;
+            }
+
+            return null;
         };
 
         function getCurrentTicks(player) {
