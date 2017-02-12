c67137cd6c64f6bbbc848f3634fe6e8d6d0baded
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-02-11 19:05:30 -0500

    update recordingcreator

--- viewmanager/viewcontainer-lite.js
+++ viewmanager/viewcontainer-lite.js
@@ -1,4 +1,4 @@
-define(['browser', 'dom', 'css!./viewcontainer-lite'], function (browser, dom) {
+define(['browser', 'dom', 'layoutManager', 'css!./viewcontainer-lite'], function (browser, dom, layoutManager) {
     'use strict';
 
     var mainAnimatedPages = document.querySelector('.mainAnimatedPages');
@@ -164,16 +164,17 @@ define(['browser', 'dom', 'css!./viewcontainer-lite'], function (browser, dom) {
 
         return new Promise(function (resolve, reject) {
 
-            var duration = 400;
+            var duration = layoutManager.tv ? 450 : 160;
             var animations = [];
+
+            newAnimatedPage.style.opacity = 0;
+            setAnimation(newAnimatedPage, 'view-fadein ' + duration + 'ms ease-in normal both');
+            animations.push(newAnimatedPage);
 
             if (oldAnimatedPage) {
                 setAnimation(oldAnimatedPage, 'view-fadeout ' + duration + 'ms ease-out normal both');
                 animations.push(oldAnimatedPage);
             }
-
-            setAnimation(newAnimatedPage, 'view-fadein ' + duration + 'ms ease-in normal both');
-            animations.push(newAnimatedPage);
 
             currentAnimations = animations;
 
