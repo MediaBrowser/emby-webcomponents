8f0467c9477ca3eee3e0c1dc5f3507b209a8262d
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-06-18 14:43:27 -0400

    add mp2 for tizen

--- browserdeviceprofile.js
+++ browserdeviceprofile.js
@@ -332,7 +332,7 @@ define(['browser'], function (browser) {
             videoTestElement.canPlayType('video/mp4; codecs="avc1.640029, mp4a.6B"').replace(/no/, '');
 
         // Not sure how to test for this
-        var supportsMp2VideoAudio = browser.edgeUwp;
+        var supportsMp2VideoAudio = browser.edgeUwp || browser.tizen;
 
         // Only put mp3 first if mkv support is there
         // Otherwise with HLS and mp3 audio we're seeing some browsers
