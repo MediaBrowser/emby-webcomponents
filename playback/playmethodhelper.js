d6474c973d71327d7c1c86f1c3bdb750f305821b
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-06-25 13:44:43 -0400

    jshint fixes

--- playback/playmethodhelper.js
+++ playback/playmethodhelper.js
@@ -6,13 +6,13 @@
         if (session.TranscodingInfo && session.TranscodingInfo.IsVideoDirect) {
             return 'DirectStream';
         }
-        else if (session.PlayState.PlayMethod == 'Transcode') {
+        else if (session.PlayState.PlayMethod === 'Transcode') {
             return 'Transcode';
         }
-        else if (session.PlayState.PlayMethod == 'DirectStream') {
+        else if (session.PlayState.PlayMethod === 'DirectStream') {
             return 'DirectPlay';
         }
-        else if (session.PlayState.PlayMethod == 'DirectPlay') {
+        else if (session.PlayState.PlayMethod === 'DirectPlay') {
             return 'DirectPlay';
         }
     }
