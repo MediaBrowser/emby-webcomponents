264b63b3e60f86118add969cbe76ad3b56ca49a9
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-10-02 13:29:11 -0400

    add error handling

--- homesections/homesections.js
+++ homesections/homesections.js
@@ -855,6 +855,9 @@
             elem.innerHTML = html;
 
             imageLoader.lazyChildren(elem);
+        }, function () {
+            // handle older servers
+            return Promise.resolve();
         });
     }
 
