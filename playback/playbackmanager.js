dfd872336074ad4e80565573913fb59814f7eaf1
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-01-30 13:21:54 -0500

    jshint fix

--- playback/playbackmanager.js
+++ playback/playbackmanager.js
@@ -561,7 +561,7 @@
 
                 if (!endpointInfo.IsLocal) {
                     var path = (mediaSource.Path || '').toLowerCase();
-                    if (path.indexOf('localhost') != -1 || path.indexOf('127.0.0.1') != -1) {
+                    if (path.indexOf('localhost') !== -1 || path.indexOf('127.0.0.1') !== -1) {
                         // This will only work if the app is on the same machine as the server
                         return Promise.resolve(false);
                     }
